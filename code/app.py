6d46ba5b596cfda390e325b9ed2f696e6a1e69bf
Nathan <nathanjwtx@nathanjwtx.io>
2019-06-22 16:01:42 -0500

    Updated app.py

--- code/app.py
+++ code/app.py
@@ -30,12 +30,12 @@ api.add_resource(Items, "/items")
 api.add_resource(UserRegister, "/register")
 api.add_resource(StoreList, "/stores")
 
-from db import db
-db.init_app(app)
+# from db import db
+# db.init_app(app)
 
 if __name__ == "__main__":
     # sqlalchemy must be imported here to ensure it is only imported once
     # at run time and not each time app.py is called/imported
-    # from db import db
-    # db.init_app(app)
+    from db import db
+    db.init_app(app)
     app.run()
